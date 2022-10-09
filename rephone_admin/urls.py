95dcd43440c142668fc496f9716906c732685a8b
RISHEL PK <rishelpk007@gmail.com>
2022-10-09 23:35:13 +0530

    dash_update

--- rephone_admin/urls.py
+++ rephone_admin/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('a1',views.das,name='dash')
+    path('a',views.das,name='dash'),
+    path('a1',views.admin_dash,name='admin_dash'),
+    path('a2',views.addpro,name='addpro'),
+    path('a3',views.product_list,name='pro_list'),
+    path('a4',views.manage_order,name='manage_order'),
+    path('login',views.login,name='login')
+ 
+
 
 
 ]
--- rephone_admin/views.py
+++ rephone_admin/views.py
@@ -2,3 +2,26 @@ from django.shortcuts import render
 
 def das (request):
     return render(request,'admintemp/dashboard.html')
+
+def admin_dash (request):
+    return render (request,'admintemp/admin_dash.html')  
+
+def addpro (request):
+    return render (request,'admintemp/addproduct.html')    
+
+def product_list (request):
+    return render (request,'admintemp/products_list.html')        
+
+def manage_order (request):
+    return render (request,'admintemp/manageorder.html')        
+
+
+
+def login (request):
+    return render (request,'admintemp/login.html')    
+          
+
+
+
+
+ 
